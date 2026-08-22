c471a4cea6f0b335862639e341fb1cf095f05bf9
Mingli Yuan <mingli.yuan@gmail.com>
2026-08-22 17:26:47 +0800

    Migrate pendulum period-matrix vignette to analysis namespaces

--- tests/classical/test_pendulum_period_matrix.py
+++ tests/classical/test_pendulum_period_matrix.py
@@ -95,12 +95,12 @@ import math
 
 import sympy as sp
 
-from aeg_shakespeare import (
+from aeg_shakespeare.analysis.abelian import (
     AbelianCycleSystem,
     compute_period_matrix,
-    hyperelliptic_profile,
     lift_square_root_path,
 )
+from aeg_shakespeare.analysis.algebraic import hyperelliptic_profile
 
 
 def pendulum_e0_curve():
